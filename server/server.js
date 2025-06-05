c86ab64b3808bb600c7aecd54d33de30e44ea172
Stells228 <151114074+Stells228@users.noreply.github.com>
2025-06-06 00:04:23 +0800

    #

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const socketIo = require('socket.io');
-const http = require('http');
+const https = require('https');
 const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: "*",
